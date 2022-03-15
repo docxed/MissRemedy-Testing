2e7c65063ddb75e773f2bae92f8954776e931596
Vichayuth <56262507+Vichayuth@users.noreply.github.com>
2022-03-16 02:19:09 +0700

    Test_chat_sample

--- screens/PharmacistChatScreen.js
+++ screens/PharmacistChatScreen.js
@@ -1,74 +1,85 @@
 import React from "react";
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Header } from "react-native-elements";
 import { addChat, db } from "../firebase";
 import { onSnapshot, collection } from "firebase/firestore";
 
-const PharmacistChatScreen = ({route, navigation}) => {
-
-  const dataSet = route.params.router
-  const indexer = route.params.indexer
+const PharmacistChatScreen = ({ route, navigation }) => {
+  const dataSet = route.params.router;
+  const indexer = route.params.indexer;
 
   const [InputText, setInputText] = React.useState("");
   const [data, setData] = React.useState("");
-  const [ Index, setIndex ] = React.useState();
+  const [Index, setIndex] = React.useState();
 
   React.useEffect(() => {
-    console.log(indexer)
+    console.log(indexer);
 
-    const unsub = onSnapshot(collection(db, "UserChat", "1", indexer), (docs) => {
-      let arr = [];
-      docs.forEach((doc) => {
-        
-        arr.push({...doc.data(), uid: doc.id});
-      });
-      setData(arr);
-      setIndex(arr.length)
-    });
+    const unsub = onSnapshot(
+      collection(db, "UserChat", "1", indexer),
+      (docs) => {
+        let arr = [];
+        docs.forEach((doc) => {
+          arr.push({ ...doc.data(), uid: doc.id });
+        });
+        setData(arr);
+        setIndex(arr.length);
+      }
+    );
     return () => {
       unsub();
     };
   }, []);
 
   React.useEffect(() => {
-    console.log(indexer)
+    console.log(indexer);
 
-    const unsub = onSnapshot(collection(db, "UserChat", "1", indexer), (docs) => {
-      let arr = [];
-      docs.forEach((doc) => {
-        
-        arr.push({...doc.data(), uid: doc.id});
-      });
-      setData(arr);
-      setIndex(arr.length)
-    });
+    const unsub = onSnapshot(
+      collection(db, "UserChat", "1", indexer),
+      (docs) => {
+        let arr = [];
+        docs.forEach((doc) => {
+          arr.push({ ...doc.data(), uid: doc.id });
+        });
+        setData(arr);
+        setIndex(arr.length);
+      }
+    );
     return () => {
       unsub();
     };
   }, []);
 
   const renderItem = (itemData) => {
-    let item = itemData.item
-    if(item.type == "u") {
-      return(
-      <View style={styles.chatBox}>
-        <Text style={styles.text}>{item.text}</Text>
-      </View>
-    )
+    let item = itemData.item;
+    if (item.type == "u") {
+      return (
+        <View style={styles.chatBox}>
+          <Text style={styles.text}>{item.text}</Text>
+        </View>
+      );
     } else {
-      return(
+      return (
         <View style={styles.chatBoxP}>
           <Text style={styles.text}>{item.text}</Text>
         </View>
-      )
+      );
     }
-  }
+  };
 
   function addText() {
-    addChat(InputText, Index, indexer, "p")
-    setIndex(Index+1)
-    setInputText("")
+    addChat(InputText, Index, indexer, "p");
+    setIndex(Index + 1);
+    setInputText("");
   }
 
   return (
@@ -77,17 +88,32 @@ const PharmacistChatScreen = ({route, navigation}) => {
         containerStyle={styles.header}
         placement="center"
         leftComponent={
-          <Ionicons 
-            name="arrow-back-outline" size={40}
-            onPress={ () => {
-              navigation.navigate("PharmacistHomeScreen", {id: dataSet.params.id, name: dataSet.params.name, surname: dataSet.params.surname, arr: dataSet.params.arr})
+          <Ionicons
+            name="arrow-back-outline"
+            size={40}
+            onPress={() => {
+              navigation.navigate("PharmacistHomeScreen", {
+                id: dataSet.params.id,
+                name: dataSet.params.name,
+                surname: dataSet.params.surname,
+                arr: dataSet.params.arr,
+              });
             }}
-          />}
-        centerComponent={<Text style={styles.headerText}>{route.params.name}</Text>}
+          />
+        }
+        centerComponent={
+          <Text style={styles.headerText}>{route.params.name}</Text>
+        }
         rightComponent={
           <TouchableOpacity
-            onPress={ () => {
-              navigation.navigate("PharmacistUserDataScreen", {id: dataSet.params.id, name: dataSet.params.name, surname: dataSet.params.surname, arr: dataSet.params.arr, router: dataSet})
+            onPress={() => {
+              navigation.navigate("PharmacistUserDataScreen", {
+                id: dataSet.params.id,
+                name: dataSet.params.name,
+                surname: dataSet.params.surname,
+                arr: dataSet.params.arr,
+                router: dataSet,
+              });
             }}
           >
             <Ionicons name="information-circle-outline" size={40}></Ionicons>
@@ -96,21 +122,22 @@ const PharmacistChatScreen = ({route, navigation}) => {
       />
       <ScrollView>
         <View style={styles.rightAlign}>
-          <FlatList
-            data={data}
-            renderItem={renderItem}
-          />
+          <FlatList data={data} renderItem={renderItem} />
         </View>
       </ScrollView>
       <View style={styles.intoRow}>
-        <TextInput 
+        <TextInput
           style={styles.input}
           placeholder={"ข้อความ..."}
           onChangeText={(username) => setInputText(username)}
           value={InputText}
         />
         <TouchableOpacity>
-          <Ionicons name="arrow-forward-circle-outline" size={40} onPress={addText}></Ionicons>
+          <Ionicons
+            name="arrow-forward-circle-outline"
+            size={40}
+            onPress={addText}
+          ></Ionicons>
         </TouchableOpacity>
       </View>
     </View>
@@ -170,7 +197,7 @@ const styles = StyleSheet.create({
     paddingVertical: "10px",
     borderRadius: "10px",
     marginBottom: "5px",
-  }
+  },
 });
 
 export default PharmacistChatScreen;
